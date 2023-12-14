52bb8996e1fcf04f0ca02f50b033c53d3213de07
Viihctor <131902065+Viihctor@users.noreply.github.com>
2023-12-13 23:41:51 -0300

    Update main.c
    BugFixing- laço de repetição mal posicionado.

--- CODIGO/main.c
+++ CODIGO/main.c
@@ -9,17 +9,19 @@ vendas do dia, gerar um relatório de meias em estoque e um relatório de vendas
 do dia com o lucro obtido.
 */
 
-int quantidade=0, preco=1, custo=2, venda=3;
+int quantidade = 0, preco = 1, custo = 2, venda = 3;
 float Produtos[4][4]; // Matriz Globalizada de estoque
 float Relatorio[4];   // Matriz Globalizada para armazenagem de dados relevantes
 const char *categorias[] = {"Infantis Lisas", "Infantis Estampadas",
                             "Adultas Lisas", "Adultas Estampadas"};
 
 // Função para Inicializar o Programa
+// preenche os dados da matriz com valores do input
+
 int inicializarEstoque() {
   int i;
   for (i = 0; i < 4; i++) {
-    
+
     printf("Forneça a quantidade de Meias %s:\n--> ", categorias[i]);
     scanf("%f", &Produtos[i][quantidade]);
 
@@ -33,18 +35,26 @@ int inicializarEstoque() {
   }
 
   // Formatação dos Relatorios
-  for(i = 0; i < 4; i++) {
-    Relatorio[i]=0;
-    Produtos[i][venda]=0;
+  // prara inicializar as variaveis zeradas
+
+  for (i = 0; i < 4; i++) {
+    Relatorio[i] = 0;
+    Produtos[i][venda] = 0;
+  }
+  for (int rep = 0; rep < 4; rep++) {
+    Relatorio[2] += Produtos[rep][quantidade] * Produtos[rep][custo];
   }
 }
 
 // função para Imprimir o Valores do Dia
+// Imprime na tela os valores de cada categoria
+
 int relatorioVendasELucro() {
   int x;
   printf("\nRelatorio:\n");
-  for(x = 0; x < 4; x++) {
-    printf("%.0f Vendas de %s = R$%.2f\n", Produtos[x][venda], categorias[x], Produtos[x][preco]*Produtos[x][venda]);
+  for (x = 0; x < 4; x++) {
+    printf("%.0f Vendas de %s = R$%.2f\n", Produtos[x][venda], categorias[x],
+           Produtos[x][preco] * Produtos[x][venda]);
   }
   printf("\nArrecadação Bruta = R$%.2f\n", Relatorio[1]);
   printf("Custo Total de Produção = R$%.2f\n", Relatorio[2]);
@@ -54,57 +64,69 @@ int relatorioVendasELucro() {
 }
 
 // Função para Imprimir o Estoque
+// Imprime na tela os valores da quantidade restante de cada produto
+
 int relatorioEstoque() {
   printf("\nEstoque:\n");
-  printf("Infantil Lisa = %.0f\nInfantil Estampada = %.0f\nAdulta Lisa = %.0f\nAdulta Estampada = %.0f\n" ,Produtos[0][quantidade], Produtos[1][quantidade], Produtos[2][quantidade], Produtos[3][quantidade]);
+  printf("Infantil Lisa = %.0f\nInfantil Estampada = %.0f\nAdulta Lisa = "
+         "%.0f\nAdulta Estampada = %.0f\n",
+         Produtos[0][quantidade], Produtos[1][quantidade],
+         Produtos[2][quantidade], Produtos[3][quantidade]);
 
   printf("\n");
 }
 
 // Função que realiza Alteração no Estoque + Relatorio
-int registraVenda(float quantidadeDaVenda,int rep) {
-  rep = rep-1;
-  // Redução de Estoque Especifica
-  Produtos[rep][quantidade] -= quantidadeDaVenda;
-  // Quantidade de Venda Especifica
-  Produtos[rep][venda] += quantidadeDaVenda;
-  // Quantidade de Venda Totais
-  Relatorio[0] += quantidadeDaVenda;
-  // Lucro Bruto
-  Relatorio[1] += quantidadeDaVenda*Produtos[rep][preco];
-  // Custo Total
-  for(rep = 0; rep < 4; rep++) {
-    Relatorio[2] += Produtos[rep][quantidade]*Produtos[rep][custo];
+int registraVenda(float quantidadeDaVenda, int rep) {
+  rep = rep - 1;
+  if (Produtos[rep][quantidade] > 0 &&
+      Produtos[rep][quantidade] >= quantidadeDaVenda) {
+    // Redução de Estoque Especifica
+    Produtos[rep][quantidade] -= quantidadeDaVenda;
+    // Quantidade de Venda Especifica
+    Produtos[rep][venda] += quantidadeDaVenda;
+    // Quantidade de Venda Totais
+    Relatorio[0] += quantidadeDaVenda;
+    // Lucro Bruto
+    Relatorio[1] += quantidadeDaVenda * Produtos[rep][preco];
+    // Lucro Liquido
+    Relatorio[3] = Relatorio[1] - Relatorio[2];
+  } else {
+    printf("\nQuantidade de produtos insuficiente no estoque!\n\n");
   }
-  // Lucro Liquido
-  Relatorio[3] = Relatorio[1]-Relatorio[2];
-
   printf("\n");
 }
 
 // Função para escolha de produto
+// imprime na tela as opções e recebe a escolha do usuario
+// Checa se a escolha foi valida
+// debita no estoque de acordo com o input
+// no caso de escolha invalida ou estoque vazio, imprime na tela a mensagem de aviso
+
 int vendaProduto() {
   printf("\nEntre com o tipo de meia vendida!\n");
-  printf("(1)-Infantil Lisa == %.0f | (2)-Infantil Estampada == %.0f | (3)-Adulta Lisa == %.0f | (4)-Adulta Estampada == %.0f |\n" ,Produtos[0][quantidade], Produtos[1][quantidade], Produtos[2][quantidade], Produtos[3][quantidade]);
+  printf("(1)-Infantil Lisa == %.0f | (2)-Infantil Estampada == %.0f | "
+         "(3)-Adulta Lisa == %.0f | (4)-Adulta Estampada == %.0f |\n",
+         Produtos[0][quantidade], Produtos[1][quantidade],
+         Produtos[2][quantidade], Produtos[3][quantidade]);
 
   int opcaoVenda;
-  scanf("%d",&opcaoVenda);
+  scanf("%d", &opcaoVenda);
 
   int rep;
-  if (opcaoVenda >= 1 && opcaoVenda <= 4) {
-    for(rep = 0; rep < 4; rep++) {
+  if (opcaoVenda >= 1 && opcaoVenda < 5) {
+    for (rep = 0; rep < 4; rep++) {
       if (opcaoVenda == rep) {
         if (Produtos[rep][quantidade] > 0) {
           printf("\nDigite a quantidade de produtos vendida: ");
 
           float quantidadeDaVenda;
-          scanf("%f",&quantidadeDaVenda);
+          scanf("%f", &quantidadeDaVenda);
 
           if (Produtos[rep][quantidade] >= quantidadeDaVenda) {
             registraVenda(quantidadeDaVenda, rep);
-          }
-          else if (Produtos[rep][quantidade] < quantidadeDaVenda) {
-          printf("\nNão a produtos suficientes no estoque!\n\n");
+          } else if (Produtos[rep][quantidade] < quantidadeDaVenda) {
+            printf("\nNão a produtos suficientes no estoque!\n\n");
           }
         }
 
@@ -160,9 +182,6 @@ int main(void) {
       break;
     }
   }
-
-  return 0;
-}
 
   return 0;
 }
